e1087556753015954536e30ac5499dd7fba976df
Yunfan Xu <xuyunfan17@gmail.com>
2022-04-24 22:26:02 -0400

    update service

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -5,8 +5,7 @@ export const Services = (props) => {
         <div className='section-title'>
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          Three reasons to rent a car in XZZ
           </p>
         </div>
         <div className='row'>
